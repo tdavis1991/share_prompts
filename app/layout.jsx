222e2463c02a3c137a816b5de38571d71d70d85c
tevin davis <aod.td1105@gmail.com>
2023-07-16 01:19:32 -0400

    Working on file structure

---
+++ app/layout.jsx
@@ -0,0 +1,24 @@
+import '@styles/globals.css';
+import { Children } from 'react';
+
+export const metadata = {
+  title: 'Promptopia',
+  description: 'Discover & Share AI Prompts'
+}
+
+const RootLayout = ({ children }) => {
+  return (
+    <html lang='en'>
+      <body>
+        <div className='main'>
+          <div className='gradient' />
+          <main className='app'>
+            {children}
+          </main>
+        </div>
+      </body>
+    </html>
+  )
+}
+
+export default RootLayout
